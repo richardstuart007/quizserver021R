a891f611b105d7733962aeb02271b7b130f52100
Richard <richardstuart007@hotmail.com>
2022-11-16 15:32:42 +1300

    database 20/21

--- quizServerConstants.js
+++ quizServerConstants.js
@@ -1,41 +1,22 @@
 //
-//  Database (Railway 10)
+//  Database (Railway 20)
 //
-exports.REMOTE_KNEX_CLIENT = 'pg'
-exports.REMOTE_KNEX_PORT = 6436
-exports.REMOTE_KNEX_HOST = 'containers-us-west-71.railway.app'
-exports.REMOTE_KNEX_USER = 'postgres'
-exports.REMOTE_KNEX_PWD = 'cfMC0QKSa86EIbpBdqfk'
-exports.REMOTE_KNEX_DATABASE = 'railway'
+exports.R20R_KNEX_CLIENT = 'pg'
+exports.R20R_KNEX_PORT = 7306
+exports.R20R_KNEX_HOST = 'containers-us-west-117.railway.app'
+exports.R20R_KNEX_USER = 'postgres'
+exports.R20R_KNEX_PWD = 'xw85A7Dol3dpWGDOYdC1'
+exports.R20R_KNEX_DATABASE = 'railway'
 exports.REMOTE_URL_PORT = 9001
 //
-//  Database (Local 10)
+//  Database (Local 20)
 //
 exports.LOCAL_KNEX_CLIENT = 'pg'
 exports.LOCAL_KNEX_HOST = '127.0.0.1'
 exports.LOCAL_KNEX_USER = 'richa'
 exports.LOCAL_KNEX_PWD = 'london'
-exports.LOCAL_KNEX_DATABASE = 'Quiz010'
+exports.LOCAL_KNEX_DATABASE = 'Quiz020'
 exports.LOCAL_URL_PORT = 8001
-//
-//  Database (Local 20)
-//
-exports.LOCAL_KNEX_CLIENT2 = 'pg'
-exports.LOCAL_KNEX_HOST2 = '127.0.0.1'
-exports.LOCAL_KNEX_USER2 = 'richa'
-exports.LOCAL_KNEX_PWD2 = 'london'
-exports.LOCAL_KNEX_DATABASE2 = 'Quiz020'
-exports.LOCAL_URL_PORT2 = 8002
-//
-//  Database (Railway 20)
-//
-exports.R20R_KNEX_CLIENT2 = 'pg'
-exports.R20R_KNEX_PORT2 = 7306
-exports.R20R_KNEX_HOST2 = 'containers-us-west-117.railway.app'
-exports.R20R_KNEX_USER2 = 'postgres'
-exports.R20R_KNEX_PWD2 = 'xw85A7Dol3dpWGDOYdC1'
-exports.R20R_KNEX_DATABASE2 = 'railway'
-exports.REMOTE_URL_PORT2 = 9002
 //
 //  URL
 //
